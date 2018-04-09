5084469f1b18660a25ee01a2b097d4af6c117fc0
Michael El-Hage <bigmike1127@gmail.com>
2018-04-09 16:57:16 -0700

    make the base game

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-window.alert('sup')
+var userChoice = prompt("Rock, paper, or scissors?");
+var cpuChoice = Math.random()
+var oppChoice = "Opponent chose "
+
+if (cpuChoice <= 0.33){
+  cpuChoice = "rock";
+} else if (cpuChoice > 0.33 && cpuChoice <=0.66){
+  cpuChoice = "paper";
+} else{
+  cpuChoice = "scissors";
+}
+
+var loseString = cpuChoice + " You lose!"
+var winString = cpuChoice + " You Win!"
+
+var matchUps = function(){
+  var output = ""
+   if(userChoice === "rock"){
+     if (cpuChoice === "paper"){
+       alert(oppChoice + loseString);
+     } else if(cpuChoice === "scissors"){
+       alert(oppChoice + winString);
+     }
+
+   }
+   else if(userChoice === "paper"){
+     if(cpuChoice === "rock"){
+       alert(oppChoice + winString);
+     } else if(cpuChoice === "scissors"){
+       alert(oppChoice + loseString);
+     }
+
+   }
+   else if(userChoice === "scissors"){
+     if(cpuChoice === "rock"){
+       alert(oppChoice + loseString);
+     } else if(cpuChoice === "paper"){
+       alert(oppChoice + winString);
+     }
+   }
+   if(userChoice === cpuChoice){
+     alert("The result is a Tie!");
+   }
+} 
+
+matchUps();
+
+/*
+- [ ] (easy) - Prevent invalid user choices from being made
+- [ ] (easy) - Improve win/loss messaging, i.e. "You chose x. Computer chose y. You win/lose"
+- [ ] (medium/hard) Allow more rounds 
+- [ ] "Best 3 of 5"
+- [ ] (depends on ^) Keep score of wins and losses
+- [ ] (med/hard) allow ties to be broken
+*/
