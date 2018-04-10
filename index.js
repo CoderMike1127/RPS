6d0aba7a3411ad3a30d357d5f20e11d502216474
Michael El-Hage <bigmike1127@gmail.com>
2018-04-09 17:30:36 -0700

    improve result message

--- index.js
+++ index.js
@@ -1,49 +1,69 @@
-var userChoice = prompt("Rock, paper, or scissors?");
-var cpuChoice = Math.random()
-var oppChoice = "Opponent chose "
-
-if (cpuChoice <= 0.33){
-  cpuChoice = "rock";
-} else if (cpuChoice > 0.33 && cpuChoice <=0.66){
-  cpuChoice = "paper";
-} else{
-  cpuChoice = "scissors";
+var generateCPUChoice = function(){
+  var cpuChoice = Math.random()
+  if (cpuChoice <= 0.33){
+    return "rock";
+  } else if (cpuChoice > 0.33 && cpuChoice <=0.66){
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
-var loseString = cpuChoice + " You lose!"
-var winString = cpuChoice + " You Win!"
 
-var matchUps = function(){
-  var output = ""
-   if(userChoice === "rock"){
-     if (cpuChoice === "paper"){
-       alert(oppChoice + loseString);
-     } else if(cpuChoice === "scissors"){
-       alert(oppChoice + winString);
-     }
+var makeResultMessage = function(msgIndex, userPick, cpuPick){
+  var msgOptions = ['You lose!', 'You Win!', 'Tie!'];
+  var userOptions = ["rock", "paper", "scissors"];
+    return "You picked " + userPick + ". Your opponent picked " + cpuPick + ". " + msgOptions[msgIndex]
+}
 
-   }
-   else if(userChoice === "paper"){
-     if(cpuChoice === "rock"){
-       alert(oppChoice + winString);
-     } else if(cpuChoice === "scissors"){
-       alert(oppChoice + loseString);
-     }
 
-   }
-   else if(userChoice === "scissors"){
-     if(cpuChoice === "rock"){
-       alert(oppChoice + loseString);
-     } else if(cpuChoice === "paper"){
-       alert(oppChoice + winString);
-     }
-   }
-   if(userChoice === cpuChoice){
-     alert("The result is a Tie!");
-   }
-} 
+var RPS = function(){
+  var userChoice = prompt("Rock, paper, or scissors?");
+  var oppChoice = "Opponent chose "
+  var cpuChoice = generateCPUChoice();
+  
 
-matchUps();
+  
+
+
+  var findWinner = function() {
+   
+    var output = ""
+    if(userChoice === "rock"){
+      if (cpuChoice === "paper"){
+        output = makeResultMessage(0, userChoice, cpuChoice)
+      } else if(cpuChoice === "scissors"){
+        output = oppChoice + makeResultMessage(1, userChoice, cpuChoice);
+      }
+
+    }
+    else if(userChoice === "paper"){
+      if(cpuChoice === "rock"){
+        output = oppChoice + makeResultMessage(1, userChoice, cpuChoice);
+      } else if(cpuChoice === "scissors"){
+        output = oppChoice + makeResultMessage(0, userChoice, cpuChoice);
+      }
+
+    }
+    else if(userChoice === "scissors"){
+      if(cpuChoice === "rock"){
+        output = makeResultMessage(0, userChoice, cpuChoice);
+      } else if(cpuChoice === "paper"){
+        output = makeResultMessage(1, userChoice, cpuChoice);
+      }
+    }
+    if(userChoice === cpuChoice){
+      output = makeResultMessage(2, userChoice, cpuChoice);
+    }
+
+    alert(output);
+  } 
+
+  findWinner();
+
+}
+RPS();
+
 
 /*
 - [ ] (easy) - Prevent invalid user choices from being made
